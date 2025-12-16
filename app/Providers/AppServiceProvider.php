07940b08d454cf047eb190769ce362f00796ca1b
paulornr89 <paulornr89@gmail.com>
2025-12-15 22:14:44 -0300

    incluído arquivos para deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,36 @@
-<?php
+<?PHP 
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; 
+use Illuminate\Support\ServiceProvider; //Para erros de RateLimit, limite de requisições para a API 
+use Illuminate\Cache\RateLimiting\Limit; 
+use Illuminate\Http\Request; 
+use Illuminate\Support\Facades\RateLimiter; 
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
+class AppServiceProvider extends ServiceProvider {   
+     /**    
+      * Register any application services.     
      */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void    {        
+        //    
+    }  
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+    /**    
+     *  Bootstrap any application services.     
+     * */        
+    public function boot(): void    {        
+        //Configura limite de requisições para a API
+        $this->configureRateLimiting();         
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');        
+        }    
+    }   
+    
+    //Para limitar requisições na API por usuário ou IP    
+    protected function configureRateLimiting()   {
+        RateLimiter::for('api', function (Request $request) {            
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+    } 
 }
